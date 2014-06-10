251ee250cfa77d602e801df85b054199c123d0fd
James Hobin <jhobin@mozilla.com>
2014-06-10 11:49:15 -0700

    Add xpcshell test of logcapture

---
+++ toolkit/devtools/tests/unit/test_logcapture.js
@@ -0,0 +1,32 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+const { notEqual, ok } = Cu.import('resource://testing-common/Assert.jsm');
+const { LogCapture } = Cu.import('resources://gre/modules/devtools/LogCapture.jsm');
+
+/**
+ * Test that LogCapture successfully reads from the /dev/log devices, returning
+ * a Uint8Array of some length, including zero. This tests the standard
+ * /dev/log devices: main, system, events, and radio.
+ */
+function run_test() {
+  function verifyLog(log) {
+    // log exists
+    notEqual(log, null);
+    // log has a length and it is non-negative (is probably array-like)
+    ok(log.length >= 0);
+  }
+
+  let mainLog = LogCapture.captureLog('main');
+  verifyLog(mainLog);
+
+  let systemLog = LogCapture.captureLog('system');
+  verifyLog(systemLog);
+
+  let eventsLog = LogCapture.captureLog('events');
+  verifyLog(eventsLog);
+
+  let radioLog = LogCapture.captureLog('radio');
+  verifyLog(radioLog);
+}
+
